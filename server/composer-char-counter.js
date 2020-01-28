3a8670ef21d2b4d5b1513c511f0bdc8625199468
antosha-85 <antosha-85@yandex.ru>
2020-01-28 23:43:34 +0000

    before applying tweets

---
+++ server/composer-char-counter.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    let count = 140;
+    $("textarea").on("keypress", function() {
+        $('.counter').html(count - $(this).val().length);
+        if ($(this).val().length >= 140) {
+            $('.counter').css("color", "red");
+        }
+    })
+    
+})
